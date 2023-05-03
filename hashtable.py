2ce5d1d020056a15f78ac8dda5a7cb1f1ab280f7
me <me@robertcurtiss.com>
2023-05-03 14:06:18 -0400

    Just before refactoring for Get the Key-Value Pairs All Tests Pass

--- hashtable.py
+++ hashtable.py
@@ -1,8 +1,46 @@
 # https://realpython.com/python-hash-table/
+
+BLANK = object()
+
+
 class HashTable:
     def __init__(self, capacity) -> None:
         self.capacity = capacity
-        self.values = capacity * [None]
+        self.values = capacity * [BLANK]
 
     def __len__(self):
         return len(self.values)
+
+    def __setitem__(self, key, value):
+        self.values[self._index(key)] = value
+        # print(self.values)
+
+    def __getitem__(self, key):
+        value = self.values[self._index(key)]
+        if value is BLANK:
+            raise KeyError(key)
+        return value
+
+    def __contains__(self, key):
+        try:
+            self[key]
+        except KeyError:
+            return False
+        else:
+            return True
+
+    def get(self, key, default=None):
+        try:
+            return self[key]
+        except KeyError:
+            return default
+
+    def __delitem__(self, key):
+        # print(f"index={self._index}")
+        if key in self:
+            self[key] = BLANK
+        else:
+            raise KeyError(key)
+
+    def _index(self, key):
+        return hash(key) % len(self)
--- tests/test_hastable.py
+++ tests/test_hastable.py
@@ -1,9 +1,20 @@
 # https://realpython.com/python-hash-table/
-from hashtable import HashTable
+import pytest
+
+from hashtable import BLANK, HashTable
+
+
+@pytest.fixture
+def hash_table():
+    sample_data = HashTable(capacity=100)
+    sample_data["hola"] = "hello"
+    sample_data[98.6] = 37
+    sample_data[False] = True
+    return sample_data
 
 
 def test_always_pass():
-    assert 2 + 2 == 4, "This is a dummy test"
+    pass
 
 
 def test_should_create_hashtable():
@@ -16,9 +27,99 @@ def test_should_report_capacity():
 
 def test_should_create_empty_value_slots():
     # Given
-    expected_values = [None, None, None]
+    expected_values = [BLANK, BLANK, BLANK]
     hash_table = HashTable(capacity=3)
     # When
     actual_values = hash_table.values
     # Then
     assert actual_values == expected_values
+
+
+def test_should_insert_key_value_pairs():
+    hash_table = HashTable(capacity=100)
+    hash_table["hola"] = "hello"
+    hash_table[98.6] = 37
+    hash_table[False] = True
+    assert "hello" in hash_table.values
+    assert 37 in hash_table.values
+    assert True in hash_table.values
+    assert len(hash_table) == 100
+
+
+@pytest.mark.skip
+def test_should_not_shrink_when_removing_elements():
+    pass
+
+
+def test_should_insert_none_value():
+    hash_table = HashTable(capacity=100)
+    hash_table["key"] = None
+    assert None in hash_table.values
+
+
+def test_case_should_not_contain_none_value_when_created():
+    assert None not in HashTable(capacity=100).values
+
+
+def test_should_find_value_by_key(hash_table):
+    assert hash_table["hola"] == "hello"
+    assert hash_table[98.6] == 37
+    assert hash_table[False] == True
+
+
+def test_should_raise_error_on_missing_key():
+    hash_table = HashTable(capacity=100)
+    with pytest.raises(KeyError) as exception_info:
+        hash_table["missing key"]
+    assert exception_info.value.args[0] == "missing key"
+
+
+def test_should_find_key(hash_table):
+    assert "hola" in hash_table
+
+
+def test_not_should_find_key(hash_table):
+    assert "missing_key" not in hash_table
+
+
+def test_should_get_value(hash_table):
+    assert hash_table.get("hola") == "hello"
+
+
+def test_should_get_none_when_missing_key(hash_table):
+    assert hash_table.get("missing_key") is None
+
+
+def test_should_get_default_value_when_missing_key(hash_table):
+    assert hash_table.get("missing_key", "default") == "default"
+
+
+def test_should_get_value_with_default(hash_table):
+    assert hash_table.get("hola", "default") == "hello"
+
+
+def test_should_delete_key_value_pair(hash_table):
+    assert "hola" in hash_table
+    assert "hello" in hash_table.values
+    assert len(hash_table) == 100
+    del hash_table["hola"]
+    assert "hola" not in hash_table
+    assert "hello" not in hash_table.values
+    assert len(hash_table) == 100
+
+
+def test_should_raise_key_error_when_deleting(hash_table):
+    with pytest.raises(KeyError) as exception_info:
+        del hash_table["missing_key"]
+    assert exception_info.value.args[0] == "missing_key"
+
+
+def test_should_update_value(hash_table):
+    assert hash_table["hola"] == "hello"
+
+    hash_table["hola"] = "hallo"
+
+    assert hash_table["hola"] == "hallo"
+    assert hash_table[98.6] == 37
+    assert hash_table[False] == True
+    assert len(hash_table) == 100
